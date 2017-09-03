b47638c892fd3cb5c4c7d7b46c583c0408d01980
yavorm95 <yavormihalski@gmail.com>
2017-09-03 13:28:17 +0200

    Add files via upload

---
+++ active.js
@@ -0,0 +1,54 @@
+//Top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // Chrome, Safari & Opera 
+    document.documentElement.scrollTop = 0; // IE & Firefox
+}
+
+//Sidebar height and likes position
+var w = window.innerWidth;
+var sidebar = document.getElementsByClassName("sidebar")[0];
+var likes = document.getElementsByClassName("likes")[0];
+
+window.onload = function(){
+if(w < 993){
+    sidebar.style.height = "110vh";
+    likes.style.display = "block";
+}}
+
+
+//Twitter
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+//Facebook
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.10";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
